a13b41aa6b65bf9e4907007b8d0ca75041ec0afb
KentaroYoshizumi <schecter3.2.1-@softbank.ne.jp>
2021-06-09 18:52:58 +0000

    second commit

--- app.js
+++ app.js
@@ -1,75 +1,44 @@
-const quiz = [
-  {
-    question: <img src="img/beegle.jpg">;
-    answers: [ 'サモエド', 'ポメラニアン', 'ビーグル', 'シェルティ'],
-    correct: 'ビーグル'
-  }, {
-    question: '糸井重里が企画に関わった、任天堂の看板ゲームといえば？',
-    answers: [ 'MOTHER2', 'スーパーマリオブラザーズ3', 'スーパードンキーコング', '星のカービィ'],
-    correct: 'MOTHER2'
-  }, {
-    question: 'ファイナルファンタジーⅣの主人公の名前は？',
-    answers: [ 'フリオニール', 'クラウド', 'ティーダ', 'セシル'],
-    correct: 'セシル'
-  }
-];
+const answers = [ 
+    'サモエド', 
+    'ポメラニアン', 
+    'ビーグル', 
+    'シェルティ'
+    ];
+    
+const correct = 'ビーグル';
+const $button = document.getElementsByTagName('button');
+ 
+$button[0].textContent = answers[0];
+$button[1].textContent = answers[1];
+$button[2].textContent = answers[2];
+$button[3].textContent = answers[3];
 
-const $window = window;
-const $doc = document;
-const $question = document.images('js-question');
-const $buttons = $doc.querySelectorAll('.btn');
-
-const quizLen = quiz.length;
-let quizCount = 0;
-let score = 0;
-
-const init = () => {
-  $question.textContent = quiz[quizCount].question;
-  
-  const buttonLen = $buttons.length;
-  let btnIndex = 0;
-  
-  while(btnIndex < buttonLen){
-    $buttons[btnIndex].textContent = quiz[quizCount].answers[btnIndex];
-    btnIndex++;
-  }
-};
-
-const goToNext = () => {
-  quizCount++;
-  if(quizCount < quizLen){
-    init(quizCount);
-  } else {
-    // $window.alert('クイズ終了！');
-    showEnd();
-  }
-};
-
-const judge = (elm) => {
-  if(elm.textContent === quiz[quizCount].correct){
-    $window.alert('正解!');
-    score++;
-  } else {
-    $window.alert('不正解!');
-  }
-  goToNext();
-};
-
-const showEnd = () => {
-  $question.textContent = '終了！あなたのスコアは' + score + '/' + quizLen + 'です';
-  
-  const $items = $doc.getElementById('js-items');
-  $items.style.visibility = 'hidden';
-};
-
-init();
-
-let answersIndex = 0;
-let answersLen = quiz[quizCount].answers.length;
-
-while(answersIndex < answersLen){
-  $buttons[answersIndex].addEventListener('click', (e) => {
-    judge(e.target);
-  });
-  answersIndex++;
-}
+//正誤判定
+$button[0].addEventListener('click', () => {
+    if(correct === $button[0].textContent){
+        window.alert('正解!');
+    } else {
+        window.alert('不正解!');
+    }
+});
+$button[1].addEventListener('click', () => {
+    if(correct === $button[1].textContent){
+        window.alert('正解!');
+    } else {
+        window.alert('不正解!');
+    }
+});
+$button[2].addEventListener('click', () => {
+    if(correct === $button[2].textContent){
+        window.alert('正解!');
+    } else {
+        window.alert('不正解!');
+    }
+});
+$button[3].addEventListener('click', () => {
+    if(correct === $button[3].textContent){
+        window.alert('正解!');
+    } else {
+        window.alert('不正解!');
+    }
+});
