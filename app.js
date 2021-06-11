64964f96d1b8a1998a798dc19c1983b25c949015
KentaroYoshizumi <39523342+KentaroYoshizumi@users.noreply.github.com>
2021-06-11 22:36:23 +0900

    Update app.js

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ const answers = [
 const correct = 'ビーグル';
 
 const $button = document.getElementsByTagName('button');
+const buttonLength = $button.length;
 
   const setupQuiz = () => {
     let buttonIndex = 0;
-    let buttonLength = $button.length;
 　  while(buttonIndex < buttonLength) {
     $button[buttonIndex].textContent = answers[buttonIndex];
     buttonIndex++;
@@ -20,32 +20,19 @@ const $button = document.getElementsByTagName('button');
 
 setupQuiz();
 
+const clickHandler = (e) => {
+    if(correct === e.target.textContent){
+        window.alert('正解!');
+    } else {
+        window.alert('不正解!');
+    }
+};
+
 //正誤判定
-$button[0].addEventListener('click', (e) => {
-    if(correct === e.target.textContent){
-        window.alert('正解!');
-    } else {
-        window.alert('不正解!');
-    }
-});
-$button[1].addEventListener('click', (e) => {
-    if(correct === e.target.textContent){
-        window.alert('正解!');
-    } else {
-        window.alert('不正解!');
-    }
-});
-$button[2].addEventListener('click', (e) => {
-    if(correct === e.target.textContent){
-        window.alert('正解!');
-    } else {
-        window.alert('不正解!');
-    }
-});
-$button[3].addEventListener('click', (e) => {
-    if(correct === e.target.textContent){
-        window.alert('正解!');
-    } else {
-        window.alert('不正解!');
-    }
-});
+let handlerIndex = 0;
+while(handlerIndex < buttonLength) {
+    $button[handlerIndex].addEventListener('click', (e) => {
+        clickHandler(e);
+    });
+    handlerIndex++;
+}
